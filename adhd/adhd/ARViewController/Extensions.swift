539f796d9c4963bf3e4bf32c1690c177fdf23bd7
Colin Zhou <colinzhou@berkeley.edu>
2020-03-14 16:11:33 -0700

    First commit

---
+++ adhd/adhd/ARViewController/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  adhd
+//
+//  Created by Colin Zhou on 2020-03-14.
+//  Copyright © 2020 Olivia Li. All rights reserved.
+//
+
+import SceneKit
+
+extension SCNVector3 {
+    func length() -> Float {
+        return sqrtf(x * x + y * y + z * z)
+    }
+}
+
+func - (l: SCNVector3, r: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(l.x - r.x, l.y - r.y, l.z - r.z)
+}
+
+extension Collection where Element == CGFloat, Index == Int {
+    /// Return the mean of a list of CGFloat. Used with `recentVirtualObjectDistances`.
+    var average: CGFloat? {
+        guard !isEmpty else {
+            return nil
+        }
+        
+        let sum = reduce(CGFloat(0)) { current, next -> CGFloat in
+            return current + next
+        }
+        
+        return sum / CGFloat(count)
+    }
+}
---
+++ adhd/adhd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adhd
+//
+//  Created by Colin Zhou on 2020-03-14.
+//  Copyright © 2020 Colin Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
